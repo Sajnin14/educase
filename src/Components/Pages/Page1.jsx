a93c069898262c797f86bd6859cc0b664880804f
Sajnin14 <sajninsaima@gmail.com>
2025-03-27 22:49:31 +0600

    page 2 done

--- src/Components/Pages/Page1.jsx
+++ src/Components/Pages/Page1.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page1 = () => {
     return (
         <div className='flex justify-center'>
-            <div className="artboard phone-4 bg-[#F7F8F9] px-5 flex flex-col justify-end pb-10">
+            <div className="artboard phone-4 bg-[#F7F8F9] px-5 flex flex-col justify-end py-10">
                 <h2 className="text-2xl text-[#1D2226] mb-2">Welcome to PopX</h2>
                 <p className='text-[#1D2226] opacity-60'>Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit,</p>
                 <button className='bg-[#6C25FF] text-white rounded-md text-[18px] text-center py-3 w-full mt-7'>Create Account</button>
--- src/Components/Pages/Page2.jsx
+++ src/Components/Pages/Page2.jsx
@@ -3,7 +3,30 @@ import React from 'react';
 const Page2 = () => {
     return (
         <div className='flex justify-center'>
-            <div className="artboard phone-4 bg-[#F7F8F9]">375×812</div>
+            <div className="artboard phone-4 bg-[#F7F8F9] px-5 py-10">
+                <h3 className='text-[#1D2226] text-2xl tracking-wider mb-[14px] font-medium'>Signin to your <br /> PopX account</h3>
+                <p className='text-[#1D2226] opacity-60'>Lorem ipsum dolor sit amet,<br /> consectetur adipiscing elit,</p>
+
+                <div className="mb-4 relative mt-[33px]">
+                    <label className="absolute -top-2.5 left-3 text-sm font-semibold text-purple-700 pl-1 pr-4 bg-[#F7F8F9]">Email Address</label>
+                    <input
+
+                        placeholder="Enter email address"
+                        className="w-full p-2 border border-[#CBCBCB] bg-[#F7F8F9] rounded-md"
+                    />
+                </div>
+
+                <div className="my-4 relative">
+                    <label className="absolute -top-2.5 left-3 text-sm font-semibold text-purple-700 pl-1 pr-4 bg-[#F7F8F9]">Password</label>
+                    <input
+
+                        placeholder="Enter password"
+                        className="w-full p-2 border border-[#CBCBCB] bg-[#F7F8F9] rounded-md"
+                    />
+                </div>
+
+                <button className='bg-[#CBCBCB] text-white rounded-md text-[18px] text-center py-3 w-full mt-2'>Login</button>
+            </div>
         </div>
     );
 };
